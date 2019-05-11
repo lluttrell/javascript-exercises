0d41ae52187a2ed1bb2019100209e75d1838d400
lluttrell <richard.lluttrell@gmail.com>
2019-05-11 13:19:25 -0600

    finishedTempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp-32)/1.8000;
+  return +cTemp.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+let fTemp = 1.8000*cTemp+32;
+return +fTemp.toFixed(1);
 }
 
 module.exports = {
