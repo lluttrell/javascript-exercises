39f54133d0965e19873890e2e1e768630a2e4b90
lluttrell <richard.lluttrell@gmail.com>
2019-05-11 09:09:58 -0600

    removeFromArray Complete

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+     let inputArray = arguments[0];
+     let removeList = Array.prototype.slice.call(arguments,1);
+     let filteredList = [];
+     for (let element of inputArray) {
+         if (!(removeList.includes(element))) {
+             filteredList.push(element);
+         }
+     }
+     return filteredList;
 }
 
 module.exports = removeFromArray
